6966df1a6112d63a752a2ff99ba75b55ee393bb2
ZidanFadhlanF <zidan161104@gmail.com>
2023-09-15 15:41:30 +0700

    belajarphp

---
+++ Pertemuan 3/index.php
@@ -0,0 +1,51 @@
+<?php
+    //Ini komentar
+    /*/
+    ini komentar banyak
+    /*/
+    // ini komentar ctrl /
+
+    echo "Pertemuan 3 zidan fadhlan fadhilah";
+    echo "<br>";
+    print "Belajar HTML";
+    echo "<br>";
+    print_r("terimakasih");
+    echo "<br>";
+    var_dump("zidan f");
+    echo "<br>";
+    $nama = "Zidan Fadhlan Fadhilah";
+    $x = 12;
+    $y = 20;
+    $nama_depan = "Zidan";
+    $nama_belakang = " Fadhlan";
+    $a = 2;
+    $a = 3;
+
+    var_dump($x > 6, $y < 10);
+    echo "<br>";
+    var_dump(1 == 5);
+    echo "<br>";
+    var_dump(5 <= 5);
+    echo "<br>";
+    echo $nama_depan.$nama_belakang;
+    echo "<br>";
+    echo $x + $y;
+    echo "<br>";
+    echo $nama
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "ini php";?></h1>
+    <?php
+        echo "Zidan Fadhlan Fadhilah";
+    ?>
+</body>
+</html>
